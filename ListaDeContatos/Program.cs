404bfee0855ee97c04300fc31ccf6d8ef9d09d7a
Vinicius Mussak <vinicius.mussak@outlook.com>
2026-03-25 21:09:11 -0300

    Refactor menu options and add search functionality

--- ListaDeContatos/Program.cs
+++ ListaDeContatos/Program.cs
@@ -2,17 +2,13 @@ using ListaDeContatos;
 
 var lista = new ListaDeContato();
 
-Console.WriteLine("--------- Lista de Contatos ---------");
-Console.WriteLine("1 - Adicionar");
-Console.WriteLine("2 - Listar Todos");
-Console.WriteLine("3 - Sair");
-Console.WriteLine("-------------------------------------");
-Console.Write("Digite sua opção: ");
-
+ExibirMenu();
 var opcao = Console.ReadLine();
 
 while (opcao != "3")
 {
+    Console.Clear();
+
     if(opcao == "1")
     {
         Console.Write("Nome: ");
@@ -29,16 +25,27 @@ while (opcao != "3")
         Console.WriteLine(lista.ListarTodos());
     }
 
-    Console.WriteLine("--------- Lista de Contatos ---------");
-    Console.WriteLine("1 - Adicionar");
-    Console.WriteLine("2 - Listar Todos");
-    Console.WriteLine("3 - Sair");
-    Console.WriteLine("-------------------------------------");
-    Console.Write("Digite sua opção: ");
+    if(opcao == "2")
+    {
+        var todos = lista.ListarTodos();
+        Console.WriteLine(todos);
+    }
+
+    ExibirMenu();
     opcao = Console.ReadLine();
 }   
 
 
+void ExibirMenu()
+{
+    Console.WriteLine("--------- Lista de Contatos ---------");
+    Console.WriteLine("1 - Adicionar");
+    Console.WriteLine("2 - Listar Todos");
+    Console.WriteLine("3 - Buscar por Tipo");
+    Console.WriteLine("4 - Sair");
+    Console.WriteLine("-------------------------------------");
+    Console.Write("Digite sua opção: ");
+}
 
 
 
